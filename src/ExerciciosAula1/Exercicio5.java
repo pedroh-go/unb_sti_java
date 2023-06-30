efceaea31f2fb306aa2d78316ea927cf924771c6
Pedro Henrique <64819198+pedroh-go@users.noreply.github.com>
2023-06-30 09:02:54 -0300

    Update Exercicio5.java

--- src/ExerciciosAula1/Exercicio5.java
+++ src/ExerciciosAula1/Exercicio5.java
@@ -3,19 +3,19 @@ import java.util.Scanner;
 
 public class Exercicio5{
     	  
-    public static void main(String[] args){
-
+     public static void main(String[] args){
+    	
         Scanner scanner = new Scanner(System.in);
 
-        System.out.print("Informe uma palavra ou frase para verificar se È um PalÌndromo: ");
-        String texto = scanner.nextLine(); 
+        System.out.print("Informe uma palavra ou frase para verificar se √© um Pal√≠ndromo: ");
+        String texto = scanner.nextLine().toLowerCase().replaceAll("\\s", ""); 
         scanner.close();
 
-        String textoInvertido =  new StringBuilder(texto).reverse().toString().trim().toLowerCase();
+        String textoInvertido =  new StringBuilder(texto).reverse().toString();
 
-        String resultado = "N√O È um Palindromo!";
+        String resultado = "N√ÉO √© um Palindromo!";
         if(texto.equals(textoInvertido))
-            resultado = "È um Palindromo!";
+            resultado = "√© um Palindromo!";
     
         System.out.println("\nTexto digitado: "+ texto + "\nTexto Invertido: "+textoInvertido+"\nResultado: Texto digitado "+resultado+"\n");
     }
